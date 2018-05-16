6c6836b374a11e0978fb82e1b102af3cf63dc5ea
Horst Ritter <Contact@Ritter-it.de>
2018-05-16 11:53:55 +0200

    Map matching entries to hash-key with anonymous array

---
+++ Mapping.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use feature qw(say);
+use Data::Printer {
+	color => {
+		'regex'  => 'yellow',
+		'hash'   => 'magenta',
+		'string' => 'cyan',
+		'array'  => 'green',
+	},
+	#filters => {
+		#'DateTime' => sub { $_[0]->ymd },
+		#'SCALAR'   => sub { "oh noes, I found a scalar! $_[0]" },
+		#},
+};
+
+
+
+my @lines = <DATA>;
+my %lookup = map { $_ => undef } qw/FTXXX1 FTBAZ1/;
+
+foreach my $match (
+	grep {
+		my ($id, @data) = split /,/, $_;
+		#exists $lookup{$id};
+		$lookup{$id} = \@data if exists $lookup{$id};
+	}
+	@lines
+) {
+	print $match;
+	#$Logsped{$lookup{$id}} = [splice $match, 1, 5];
+}
+
+p %lookup;
+
+__DATA__
+FTXXX1,1001,Ritter Testing,Cisco 866
+FTFOO1,1129,Foo Limited,CheckPoint
+FTBAR1,1006,Bar Limited,Palo Alto
+FTBAZ1,1074,Baz Limited,SonicWall NSA 3200
+FTJUH1,1076,J & H Limited,StrongSwan
